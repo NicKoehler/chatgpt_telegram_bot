1445ae71171fa9a91bcf9113204fb941574067e7
NicKoehler <grillinicolavocal@gmail.com>
2023-01-19 10:33:14 +0100

    added /update and /stop command

--- src/bot.py
+++ src/bot.py
@@ -1,10 +1,14 @@
 #!venv/bin/python
 
+
+import asyncio
 import warnings
-from os import getenv
 from dotenv import load_dotenv
+from subprocess import run, PIPE
+from os import getenv, name, system
 from revChatGPT.ChatGPT import Chatbot
 from utils import get_chatgpt_response
+from aiogram.utils.exceptions import BotBlocked
 from internationalization import get_translation
 from aiogram import Bot, Dispatcher, executor, types
 
@@ -18,6 +22,7 @@ OPENAI_SESSION_TOKEN = getenv("OPENAI_SESSION_TOKEN")
 OWNER_CHAT_FILTER = lambda message: message.chat.id == OWNER_ID
 
 chatbot = Chatbot(config={"session_token": OPENAI_SESSION_TOKEN})
+loop = asyncio.get_event_loop()
 bot = Bot(token=TELEGRAM_BOT_TOKEN, parse_mode=types.ParseMode.MARKDOWN)
 dp = Dispatcher(bot)
 
@@ -46,6 +51,29 @@ async def reset_handler(message: types.Message):
     await message.answer(get_translation("reset", message.from_user.language_code))
 
 
+@dp.message_handler(OWNER_CHAT_FILTER, commands="stop")
+async def stop_handler(message: types.Message):
+    await message.answer(get_translation("stop", message.from_user.language_code))
+    loop.stop()
+
+
+@dp.message_handler(OWNER_CHAT_FILTER, commands="update")
+async def update_handler(message: types.Message):
+    await message.answer(
+        get_translation("update_load", message.from_user.language_code)
+    )
+    p = run(["git", "pull"], stdout=PIPE, stderr=PIPE, text=True)
+    if p.stdout:
+        await message.answer(p.stdout)
+    if p.stderr:
+        await message.answer(p.stderr)
+    await message.answer(
+        get_translation("update_done", message.from_user.language_code)
+    )
+    system(r".\start.ps1" if name == "nt" else r".\start.sh")
+    loop.stop()
+
+
 @dp.message_handler(OWNER_CHAT_FILTER)
 async def text_handler(message: types.Message):
     await message.answer_chat_action("typing")
@@ -60,5 +88,12 @@ async def text_handler(message: types.Message):
         await message.answer(get_translation("empty", message.from_user.language_code))
 
 
+async def ready(_):
+    try:
+        await bot.send_message(OWNER_ID, "✅ • Online")
+    except BotBlocked:
+        pass
+
+
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    executor.start_polling(dp, on_startup=ready, skip_updates=True)
--- src/internationalization.py
+++ src/internationalization.py
@@ -4,30 +4,40 @@ _strings = {
             "Hi, this is ChatGPT Bot here are the supported commands:\n\n"
             "/start · Send this message\n"
             "/reset · Reset the conversation\n"
-            "/rollback · Take the conversation back one message."
+            "/rollback · Take the conversation back one message.\n"
+            "/update · Updates the bot from github\n"
+            "/stop · Stop the bot"
         ),
         "rollback_ok": "Conversation carried back one message",
         "rollback_fail": "You are already at the beginning of the conversation",
         "reset": "Conversation has been reset",
         "empty": "Empty message",
+        "stop": "Bye 👋",
+        "update_load": "Updating..",
+        "update_done": "Updated, restarting..",
     },
     "it": {
         "start": (
             "Ciao, sono ChatGPT Bot ecco i comandi supportati:\n\n"
             "/start · Invia questo messaggio\n"
             "/reset · Resetta la conversazione\n"
-            "/rollback · Porta la conversazione indietro di un messaggio"
+            "/rollback · Porta la conversazione indietro di un messaggio\n"
+            "/update · Aggiorna il bot da github\n"
+            "/stop · Ferma il bot"
         ),
         "rollback_ok": "Conversazione riportata indietro di un messaggio",
         "rollback_fail": "Sei già all'inizio della conversazione",
         "reset": "Conversazione resettata",
         "empty": "Messaggio vuoto",
+        "stop": "Arrivederci 👋",
+        "update_load": "Aggiornamento in corso..",
+        "update_done": "Aggiornato, riavvio in corso..",
     },
 }
 
 
 def get_translation(string: str, lang_code: str):
+    # fallback to english
     if lang_code not in _strings:
-        # fallback to english
         lang_code = "en"
     return _strings[lang_code][string]
